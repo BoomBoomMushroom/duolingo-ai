f46fcf2dfec58fb609055b7eb8555961ae37c0fc
Academy-of-Code <dillioncat77@gmail.com>
2023-07-24 12:01:53 -0400

    finished the text to array!

--- text_to_arr.py
+++ text_to_arr.py
@@ -2,7 +2,20 @@ import numpy
 import string
 
 # Mainly Swedish characters because I want it to learn swedish!
-CHARACTERS = "abcdefghijklmnopqrstuvwxyzåäö " + string.digits + string.punctuation
+CHARACTERS = " abcdefghijklmnopqrstuvwxyzåäö" + string.digits + string.punctuation + "“”"
+DEFAULT_LEN = 200
+
+def convertString(input, length=DEFAULT_LEN):
+    arr = numpy.full(length, -1, dtype=int)
+    if len(input) > length: return arr
+    
+    i = 0
+    for j in input:
+        arr[i] = CHARACTERS.index( j.lower() )
+        i += 1
+    
+    return arr
 
 if __name__ == "__main__":
-    print( CHARACTERS.split("") )
+    convertString("Which one of these is “milk”? mjölk kaffe vatten")
+    #print( [*CHARACTERS] )
